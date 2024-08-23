ba304e1541ac80e4a83a8eb26128f3912a9a9b24
ChrisGalHur <christian.galvez.hurtado@outlook.es>
2024-08-23 16:08:17 +0200

    added activity Calendar with ScrollView for days

--- app/src/main/java/com/chrisgalhur/tareapp/activity/MainActivity.java
+++ app/src/main/java/com/chrisgalhur/tareapp/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.chrisgalhur.tareapp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,10 @@ public class MainActivity extends BaseActivity {
     private MyPreferences preferences;
     //endregion INJECTION
 
+    //region UI
+    private Button btnToCalendar;
+    //endregion UI
+
     //region ON_CREATE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +39,19 @@ public class MainActivity extends BaseActivity {
 
         preferences = new MyPreferences(this);
 
-
         if (preferences.isFirstTimeLaunch()) {
             launchOnboardingActivity();
             finish();
         }
+
+        btnToCalendar = findViewById(R.id.btnToCalendarMain);
+
+        btnToCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CalendarActivity.class));
+            }
+        });
     }
     //endregion ON_CREATEº
 
